f465d2d1e0e58d2653414efce7dc51266e9122ce
Denise Büning <denise24111993@gmail.com>
2023-04-24 17:42:27 +0200

    update

--- webtechSS2023/src/main.tsx
+++ webtechSS2023/src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <Header/>
-        <App />
+
       <Footer/>
   </React.StrictMode>,
 )
